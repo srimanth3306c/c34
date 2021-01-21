5259f164b3885efe6f29a60e08a4d0c145238cc0
srimanth3306c <71445498+srimanth3306c@users.noreply.github.com>
2021-01-21 19:00:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var ball,database;
+var position;
+
+function setup(){
+    database = firebase.database();
+    
+    
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+var ballposition = database.ref('ball/position');
+ballposition.on("value",readPosition,showError)
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readPosition(data){
+position = data.val();
+ball.x = position.x;
+ball.y =  position.y;
+}
+
+function showError(){
+console.log("there is an error");
+}
+
+
